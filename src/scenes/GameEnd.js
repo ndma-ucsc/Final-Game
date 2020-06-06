03ffd0e80fb83c053f7c32088e71f6d61574520e
Nathan Ma <ndma@ucsc.edu>
2020-06-06 16:56:15 -0700

    Level 3

--- src/scenes/GameEnd.js
+++ src/scenes/GameEnd.js
@@ -8,6 +8,7 @@ class GameEnd extends Phaser.Scene {
         cursors = this.input.keyboard.createCursorKeys();
         keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
         keyESC = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ESC);
+        this.add.text(game.config.width/2, game.config.height/2, "THE END", {fontSize: "50px", color: "#FACADE"}).setOrigin(0.5);
     }
 
     update(){
